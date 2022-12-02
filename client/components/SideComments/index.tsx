83c57272af3acb02ac7f8a998dc53b33f5c114ae
Alexandr Constantinov <76010869+o-tidev@users.noreply.github.com>
2022-12-02 12:52:22 +0200

    implemented possibility to hide comments

--- client/components/SideComments/index.tsx
+++ client/components/SideComments/index.tsx
@@ -4,43 +4,25 @@ import ArrowRightIcon from "@material-ui/icons/NavigateNextOutlined";
 import data from "../../../data";
 
 import styles from "./SideComments.module.scss";
-
-interface CommentItemProps {
-  user: {
-    fullname: string;
-  };
-  text: string;
-  post: {
-    title: string;
-  };
-}
-
-const CommentItem: React.FC<CommentItemProps> = ({ user, text, post }) => {
-  return (
-    <div className={styles.commentItem}>
-      <div className={styles.userInfo}>
-        <img src="https://leonardo.osnova.io/598fc957-a3f6-598c-b6f9-a033c3941d12/-/scale_crop/64x64/-/format/webp/" />
-        <a href="#">
-          <b>{user.fullname}</b>
-        </a>
-      </div>
-      <p className={styles.text}>{text}</p>
-      <a href="#">
-        <span className={styles.postTitle}>{post.title}</span>
-      </a>
-    </div>
-  );
-};
+import { CommentItem } from "./CommentItem";
+import clsx from "clsx";
 
 export const SideComments = () => {
+  const [visible, setVisible] = React.useState<boolean>(true);
+
+  const toggleVisibility = () => {
+    setVisible(!visible);
+  };
+
   return (
-    <div className={styles.root}>
-      <h3>
+    <div className={clsx(styles.root, !visible && styles.rotated)}>
+      <h3 onClick={toggleVisibility}>
         Комментарии <ArrowRightIcon />
       </h3>
-      {data.comments.popular.map((obj) => (
-        <CommentItem key={obj.id} {...obj} />
-      ))}
+      {visible &&
+        data.comments.popular.map((obj) => (
+          <CommentItem key={obj.id} {...obj} />
+        ))}
     </div>
   );
 };
