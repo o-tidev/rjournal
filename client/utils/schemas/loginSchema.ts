79833e8d6161d54a3ec80d9d8205bd38107a3b46
Alexandr Constantinov <76010869+o-tidev@users.noreply.github.com>
2022-12-07 13:03:24 +0200

    added email validation with yup

---
+++ client/utils/schemas/loginSchema.ts
@@ -0,0 +1,9 @@
+import * as yup from "yup";
+
+const LoginSchema = yup.object().shape({
+  email: yup.string().email("Incorrect email").required("Email is required"),
+  password: yup
+    .string()
+    .min(6, "Password should be at least 6 characters")
+    .required("Password is required"),
+});
