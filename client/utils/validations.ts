23f9176cc07d8817a08e077b7fc83501c40c52bb
Alexandr Constantinov <76010869+o-tidev@users.noreply.github.com>
2022-12-08 10:20:21 +0200

    refactored the validations file

---
+++ client/utils/validations.ts
@@ -0,0 +1,16 @@
+import * as yup from "yup";
+
+export const LoginSchema = yup.object().shape({
+  email: yup.string().email("Incorrect email").required("Email is required"),
+  password: yup
+    .string()
+    .min(6, "Password should be at least 6 characters")
+    .required("Password is required"),
+});
+
+export const RegisterSchema = yup
+  .object()
+  .shape({
+    fullname: yup.string().required("Fullname is required"),
+  })
+  .concat(LoginSchema);
