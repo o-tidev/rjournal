0e119cda0f11b569d8e788552391bbebdda13be2
Alexandr Constantinov <76010869+o-tidev@users.noreply.github.com>
2022-12-08 14:20:44 +0200

    connected to database, mock up user creation

--- api/src/user/dto/create-user.dto.ts
+++ api/src/user/dto/create-user.dto.ts
@@ -1,1 +1,5 @@
-export class CreateUserDto {}
+export class CreateUserDto {
+  fullname: string;
+  email: string;
+  password?: string;
+}
--- api/src/user/user.service.ts
+++ api/src/user/user.service.ts
@@ -1,22 +1,30 @@
 import { Injectable } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { UserEntity } from './entities/user.entity';
 
 @Injectable()
 export class UserService {
-  create(createUserDto: CreateUserDto) {
-    return 'This action adds a new user';
+  constructor(
+    @InjectRepository(UserEntity)
+    private repository: Repository<UserEntity>,
+  ) {}
+
+  create(dto: CreateUserDto) {
+    return this.repository.save(dto);
   }
 
   findAll() {
-    return `This action returns all user`;
+    return this.repository.find();
   }
 
   findOne(id: number) {
     return `This action returns a #${id} user`;
   }
 
-  update(id: number, updateUserDto: UpdateUserDto) {
+  update(id: number, dto: UpdateUserDto) {
     return `This action updates a #${id} user`;
   }
 
