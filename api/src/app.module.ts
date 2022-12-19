f9b5550e99f1d69dcf43e1fcf3a9f98283cc3594
Alexandr Constantinov <76010869+o-tidev@users.noreply.github.com>
2022-12-19 13:48:08 +0200

    created auth endpoints

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -9,6 +9,7 @@ import { PostEntity } from './post/entities/post.entity';
 import { CommentModule } from './comment/comment.module';
 import { CommentEntity } from './comment/entities/comment.entity';
 import { AuthModule } from './auth/auth.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [
