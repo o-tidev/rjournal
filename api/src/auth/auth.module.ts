e17465bdfb2b1d54667b4cc7f0ecefeb8729919f
Alexandr Constantinov <76010869+o-tidev@users.noreply.github.com>
2022-12-20 12:48:47 +0200

    added passport module to auth module

--- api/src/auth/auth.module.ts
+++ api/src/auth/auth.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { UserModule } from 'src/user/user.module';
+import { PassportModule } from '@nestjs/passport';
 
 @Module({
-  imports: [UserModule],
+  imports: [UserModule, PassportModule],
   controllers: [AuthController],
   providers: [AuthService],
 })
