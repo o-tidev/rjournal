1e8260d235284a239ed4261ed9ca554629d3fe18
Alexandr Constantinov <76010869+o-tidev@users.noreply.github.com>
2022-12-20 12:35:08 +0200

    implemented validateUser method to auth service

--- api/src/auth/auth.service.ts
+++ api/src/auth/auth.service.ts
@@ -1,26 +1,19 @@
 import { Injectable } from '@nestjs/common';
-import { CreateAuthDto } from './dto/create-auth.dto';
-import { UpdateAuthDto } from './dto/update-auth.dto';
+import { UserService } from 'src/user/user.service';
 
 @Injectable()
 export class AuthService {
-  create(createAuthDto: CreateAuthDto) {
-    return 'This action adds a new auth';
-  }
+  constructor(private usersService: UserService) {}
 
-  findAll() {
-    return `This action returns all auth`;
-  }
-
-  findOne(id: number) {
-    return `This action returns a #${id} auth`;
-  }
-
-  update(id: number, updateAuthDto: UpdateAuthDto) {
-    return `This action updates a #${id} auth`;
-  }
-
-  remove(id: number) {
-    return `This action removes a #${id} auth`;
+  async validateUser(email: string, password: string): Promise<any> {
+    const user = await this.usersService.findByCondition({
+      email,
+      password,
+    });
+    if (user && user.password === password) {
+      const { password, ...result } = user;
+      return result;
+    }
+    return null;
   }
 }
