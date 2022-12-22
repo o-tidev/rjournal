ec1eb156683772abd2869a88690e20e75ef6b64a
Alexandr Constantinov <76010869+o-tidev@users.noreply.github.com>
2022-12-22 10:47:42 +0200

    added JWT module

--- api/src/auth/auth.module.ts
+++ api/src/auth/auth.module.ts
@@ -4,9 +4,17 @@ import { AuthController } from './auth.controller';
 import { UserModule } from 'src/user/user.module';
 import { PassportModule } from '@nestjs/passport';
 import { LocalStrategy } from './strategies/local.strategy';
+import { JwtModule } from '@nestjs/jwt';
 
 @Module({
-  imports: [UserModule, PassportModule],
+  imports: [
+    UserModule,
+    PassportModule,
+    JwtModule.register({
+      secret: 'TEST',
+      signOptions: { expiresIn: '15d' },
+    }),
+  ],
   controllers: [AuthController],
   providers: [AuthService, LocalStrategy],
 })
