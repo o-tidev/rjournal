724fa7b26ed23cf375f19e314b1f8c10b6a9c59e
Alexandr Constantinov <76010869+o-tidev@users.noreply.github.com>
2022-12-22 10:56:34 +0200

    implemented auth with JWT

--- api/src/auth/strategies/local.strategy.ts
+++ api/src/auth/strategies/local.strategy.ts
@@ -2,15 +2,10 @@ import { Strategy } from 'passport-local';
 import { PassportStrategy } from '@nestjs/passport';
 import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { AuthService } from '../auth.service';
-import { UserEntity } from 'src/user/entities/user.entity';
-import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
 export class LocalStrategy extends PassportStrategy(Strategy) {
-  constructor(
-    private authService: AuthService,
-    private jwtService: JwtService,
-  ) {
+  constructor(private authService: AuthService) {
     super({ usernameField: 'email', passwordField: 'password' });
   }
 
@@ -20,12 +15,5 @@ export class LocalStrategy extends PassportStrategy(Strategy) {
       throw new UnauthorizedException('Incorrect email or password');
     }
     return user;
-  }
-
-  async login(user: UserEntity) {
-    const payload = { email: user.email, sub: user.id };
-    return {
-      access_token: this.jwtService.sign(payload),
-    };
   }
 }
