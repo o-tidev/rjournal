34c9c865331d8a9a3dc8735a25266fcf212416ef
Alexandr Constantinov <76010869+o-tidev@users.noreply.github.com>
2022-12-22 10:47:26 +0200

    removed unused import

--- api/src/auth/auth.controller.ts
+++ api/src/auth/auth.controller.ts
@@ -1,12 +1,12 @@
 import { Controller, Post, UseGuards, Request } from '@nestjs/common';
-import { AuthGuard } from '@nestjs/passport';
 import { AuthService } from './auth.service';
+import { LocalAuthGuard } from './guards/local-auth.guard';
 
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
-  @UseGuards(AuthGuard('local'))
+  @UseGuards(LocalAuthGuard)
   @Post('login')
   async login(@Request() req) {
     return req.user;
