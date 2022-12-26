724f6ff04d11a5416ba605b82be941527ce4d7a1
Alexandr Constantinov <76010869+o-tidev@users.noreply.github.com>
2022-12-26 15:09:00 +0200

    imported user service to auth controller, created an endpoint for

--- api/src/auth/auth.controller.ts
+++ api/src/auth/auth.controller.ts
@@ -1,16 +1,26 @@
 import { Controller, Post, UseGuards, Request, Get } from '@nestjs/common';
+import { CreateUserDto } from 'src/user/dto/create-user.dto';
+import { UserService } from 'src/user/user.service';
 import { AuthService } from './auth.service';
 import { JwtAuthGuard } from './guards/jwt-auth.guard';
 import { LocalAuthGuard } from './guards/local-auth.guard';
 
 @Controller('auth')
 export class AuthController {
-  constructor(private readonly authService: AuthService) {}
+  constructor(
+    private readonly authService: AuthService,
+    private readonly userService: UserService,
+  ) {}
 
   @UseGuards(LocalAuthGuard)
   @Post('login')
   async login(@Request() req) {
     return this.authService.login(req.user);
+  }
+
+  @Post('register')
+  register(dto: CreateUserDto) {
+    return this.userService.create(dto);
   }
 
   @UseGuards(JwtAuthGuard)
