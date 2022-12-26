0bf361183a210bb283661a527b3ebaca29cccdb5
Alexandr Constantinov <76010869+o-tidev@users.noreply.github.com>
2022-12-26 16:13:00 +0200

    removed unused user service

--- api/src/auth/auth.controller.ts
+++ api/src/auth/auth.controller.ts
@@ -7,17 +7,13 @@ import {
   Body,
 } from '@nestjs/common';
 import { CreateUserDto } from 'src/user/dto/create-user.dto';
-import { UserService } from 'src/user/user.service';
 import { AuthService } from './auth.service';
 import { JwtAuthGuard } from './guards/jwt-auth.guard';
 import { LocalAuthGuard } from './guards/local-auth.guard';
 
 @Controller('auth')
 export class AuthController {
-  constructor(
-    private readonly authService: AuthService,
-    private readonly userService: UserService,
-  ) {}
+  constructor(private readonly authService: AuthService) {}
 
   @UseGuards(LocalAuthGuard)
   @Post('login')
